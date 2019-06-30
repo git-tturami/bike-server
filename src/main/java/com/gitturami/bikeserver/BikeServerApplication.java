5619a6aedb6b8860b4c1734e4c827021caa8d003
boohyunsik <bhs9194@nate.com>
2019-06-30 16:33:26 +0900

    init: initial commit

---
+++ src/main/java/com/gitturami/bikeserver/BikeServerApplication.java
@@ -0,0 +1,13 @@
+package com.gitturami.bikeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitturami/bikeserver/BikeServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gitturami.bikeserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BikeServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+}

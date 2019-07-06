c60cb6acc18abd7e9855b1f36b79d25d6dc6e197
boohyunsik <bhs9194@nate.com>
2019-07-06 19:08:33 +0900

    del: unused code in BikeServerApplicationTest

--- src/test/java/com/gitturami/bikeserver/BikeServerApplicationTest.java
+++ src/test/java/com/gitturami/bikeserver/BikeServerApplicationTest.java
@@ -8,9 +8,4 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BikeServerApplicationTest {
-
-	@Test
-	public void contextLoads() {
-	}
-
 }

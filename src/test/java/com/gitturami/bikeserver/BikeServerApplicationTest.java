73f3b1d199a490e685ae20aa2bf8310c6d209ac9
boohyunsik <bhs9194@nate.com>
2019-07-06 16:11:23 +0900

    fix: renaming test class

--- src/test/java/com/gitturami/bikeserver/BikeServerApplicationTest.java
+++ src/test/java/com/gitturami/bikeserver/BikeServerApplicationTest.java
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BikeServerApplicationTests {
+public class BikeServerApplicationTest {
 
 	@Test
 	public void contextLoads() {
 	}
-
 
 }

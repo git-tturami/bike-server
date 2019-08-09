fd1ddb2bfa96596b57a7f5281dcb0dd34d96f1cd
boohyunsik <bhs9194@nate.com>
2019-08-09 21:57:03 +0900

    fix: change package of StationNotFoundException

--- src/main/java/com/gitturami/bikeserver/controller/station/exception/StationNotFoundException.java
+++ src/main/java/com/gitturami/bikeserver/controller/station/exception/StationNotFoundException.java
@@ -1,4 +1,4 @@
-package com.gitturami.bikeserver.controller.exception;
+package com.gitturami.bikeserver.controller.station.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;

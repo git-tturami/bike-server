50f42df8ce1cf87eb001c9cec9cb391c8692da12
Minji Hwang <jjparson93@gmail.com>
2019-08-11 17:26:17 +0900

    fix: Move annotation restaurant Controller

--- src/main/java/com/gitturami/bikeserver/controller/restaurant/RestaurantController.java
+++ src/main/java/com/gitturami/bikeserver/controller/restaurant/RestaurantController.java
@@ -1,22 +1,21 @@
 package com.gitturami.bikeserver.controller.restaurant;
 
+import com.gitturami.bikeserver.controller.restaurant.exception.RestaurantNotFoundException;
 import com.gitturami.bikeserver.infra.restaurant.Impl.RestaurantApiImpl;
 import com.gitturami.bikeserver.infra.restaurant.RestaurantApi;
-import com.gitturami.bikeserver.infra.restaurant.repository.RestaurantResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/restaurants")
 public class RestaurantController {
     private RestaurantApi restaurantApi = new RestaurantApiImpl();
 
-    @RequestMapping(value= "/restaurants", method= RequestMethod.GET)
-    @ResponseStatus(value= HttpStatus.OK)
+    @GetMapping("")
     public String getTotalRestaurantList() {
         String result = restaurantApi.getTotalRestaurantList();
 
         if (result == null) {
-//            throw new StationNotFoundException();
+            throw new RestaurantNotFoundException();
         }
         return result;
     }

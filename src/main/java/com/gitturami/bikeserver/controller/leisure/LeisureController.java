e4a7abbad05423916b5b8882b35f7dcbdb6a11f1
Hyunsik Boo <bhs9194@nate.com>
2019-08-11 17:29:34 +0900

    impl: LeisureController and tcs (#41)

--- src/main/java/com/gitturami/bikeserver/controller/leisure/LeisureController.java
+++ src/main/java/com/gitturami/bikeserver/controller/leisure/LeisureController.java
@@ -5,13 +5,15 @@ import com.gitturami.bikeserver.infra.leisure.LeisureApi;
 import com.gitturami.bikeserver.infra.leisure.constants.ContentTypeIds;
 import com.gitturami.bikeserver.infra.leisure.impl.LeisureApiImpl;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/leisure")
 public class LeisureController {
-    private LeisureApi leisureApi;
+    private final LeisureApi leisureApi;
 
     public LeisureController() {
         leisureApi = new LeisureApiImpl();
@@ -21,56 +23,47 @@ public class LeisureController {
         leisureApi = api;
     }
 
-    @RequestMapping(value="/leisure")
-    @ResponseStatus(value= HttpStatus.OK)
+    @GetMapping("")
     public String getLeisureList() {
         return leisureApi.getAllLeisureInformation().toString();
     }
 
-    @RequestMapping(value="/leisure/parks")
-    @ResponseStatus(value=HttpStatus.OK)
+    @GetMapping("/parks")
     public String getParkList() {
         return leisureApi.getLeisureInformationByContentId(ContentTypeIds.PARK).toString();
     }
 
-    @RequestMapping(value="/leisure/cultures")
-    @ResponseStatus(value=HttpStatus.OK)
+    @GetMapping("/cultures")
     public String getCultureList() {
         return leisureApi.getLeisureInformationByContentId(ContentTypeIds.CULTURE).toString();
     }
 
-    @RequestMapping(value="/leisure/festivals")
-    @ResponseStatus(value=HttpStatus.OK)
+    @GetMapping("/festivals")
     public String getFestivalList() {
         return leisureApi.getLeisureInformationByContentId(ContentTypeIds.FESTIVAL).toString();
     }
 
-    @RequestMapping(value="/leisure/leports")
-    @ResponseStatus(value=HttpStatus.OK)
+    @GetMapping("/leports")
     public String getLeportList() {
         return leisureApi.getLeisureInformationByContentId(ContentTypeIds.LEPORTS).toString();
     }
 
-    @RequestMapping(value="/leisure/courses")
-    @ResponseStatus(value=HttpStatus.OK)
+    @GetMapping("/courses")
     public String getCourseList() {
         return leisureApi.getLeisureInformationByContentId(ContentTypeIds.COURSE).toString();
     }
 
-    @RequestMapping(value="/leisure/hotels")
-    @ResponseStatus(value=HttpStatus.OK)
+    @GetMapping("/hotels")
     public String getHotelList() {
         return leisureApi.getLeisureInformationByContentId(ContentTypeIds.HOTEL).toString();
     }
 
-    @RequestMapping(value="/leisure/shopping")
-    @ResponseStatus(value=HttpStatus.OK)
+    @GetMapping("/shopping")
     public String getShoppingList() {
         return leisureApi.getLeisureInformationByContentId(ContentTypeIds.SHOPPING).toString();
     }
 
-    @RequestMapping(value="/leisure/foods")
-    @ResponseStatus(value=HttpStatus.OK)
+    @GetMapping("/foods")
     public String getFoodList() {
         return leisureApi.getLeisureInformationByContentId(ContentTypeIds.FOOD).toString();
     }

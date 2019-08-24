b21cd22183c21b3f636647f7062a0913664832f8
Minji Hwang <jjparson93@gmail.com>
2019-08-24 18:26:38 +0900

    fix: Using DI

--- src/main/java/com/gitturami/bikeserver/infra/restaurant/Impl/RestaurantApiImpl.java
+++ src/main/java/com/gitturami/bikeserver/infra/restaurant/Impl/RestaurantApiImpl.java
@@ -1,31 +1,23 @@
 package com.gitturami.bikeserver.infra.restaurant.Impl;
 
+import com.gitturami.bikeserver.config.RetrofitConfig;
 import com.gitturami.bikeserver.infra.logger.ApiLogger;
 import com.gitturami.bikeserver.infra.restaurant.RestaurantApi;
-import com.gitturami.bikeserver.infra.restaurant.repository.RestaurantRepo;
 import com.gitturami.bikeserver.infra.restaurant.repository.RestaurantResponse;
-import com.gitturami.bikeserver.infra.restaurant.retrofit.RestaurantRetrofit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import retrofit2.Call;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
-import java.util.List;
 
+@Service
 public class RestaurantApiImpl implements RestaurantApi {
 
     private static final String TAG = "RestaurantApiImpl";
-    private Retrofit retrofit;
-    private RestaurantRetrofit restaurantRetrofit;
 
-    public RestaurantApiImpl() {
-        retrofit = new Retrofit.Builder()
-                .baseUrl("http://openapi.seoul.go.kr:8088/4b64546d6862687339384d4b625366/json/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        restaurantRetrofit = retrofit.create(RestaurantRetrofit.class);
-    }
+    @Autowired
+    private RetrofitConfig retrofitConfig;
 
     @Override
     public String getTotalRestaurantList() {
@@ -35,7 +27,7 @@ public class RestaurantApiImpl implements RestaurantApi {
 
     @Override
     public RestaurantResponse getRestaurantList(int startPage, int endPage) {
-        Call<RestaurantResponse> call = restaurantRetrofit.restaurantList(startPage, endPage);
+        Call<RestaurantResponse> call = retrofitConfig.getRestaurantRetrofit().restaurantList(startPage, endPage);
         try {
             Response<RestaurantResponse> response = call.execute();
             RestaurantResponse body = response.body();
--- src/test/java/com/gitturami/bikeserver/infra/restaurant/impl/RestaurantApiImplTest.java
+++ src/test/java/com/gitturami/bikeserver/infra/restaurant/impl/RestaurantApiImplTest.java
@@ -6,18 +6,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(PowerMockRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @PowerMockIgnore("javax.net.ssl.*")
 public class RestaurantApiImplTest {
 
+    @Autowired
     private RestaurantApiImpl api;
-
-    @Before
-    public void setUp() {
-        api = new RestaurantApiImpl();
-    }
 
     @Test
     public void testGetRestaurantList() {

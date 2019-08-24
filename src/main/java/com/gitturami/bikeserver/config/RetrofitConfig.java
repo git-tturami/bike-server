1f803314bc2d6d1f215cf36d75ef151787c7997d
Minji Hwang <jjparson93@gmail.com>
2019-08-24 18:26:38 +0900

    fix: Using DI

--- src/main/java/com/gitturami/bikeserver/config/RetrofitConfig.java
+++ src/main/java/com/gitturami/bikeserver/config/RetrofitConfig.java
@@ -1,19 +1,25 @@
 package com.gitturami.bikeserver.config;
 
 import com.gitturami.bikeserver.infra.bike.retrofit.BikeRetrofit;
+import com.gitturami.bikeserver.infra.restaurant.retrofit.RestaurantRetrofit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.util.ResourceBundle;
+
 @Component
 public class RetrofitConfig {
     private BikeRetrofit bikeRetrofit;
+    private RestaurantRetrofit restaurantRetrofit;
 
     @Value("${url.bikeUrl}")
     private String bikeUrl;
     @Value("${url.tourUrl}")
     private String tourUrl;
+    @Value("${url.restaurantUrl}")
+    private String restaurantUrl;
 
     public RetrofitConfig() {
         System.out.println("retrofit config");
@@ -41,5 +47,21 @@ public class RetrofitConfig {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(BikeRetrofit.class);
+    }
+
+    public RestaurantRetrofit getRestaurantRetrofit() {
+        if (restaurantRetrofit == null) {
+            setRestaurantRetrofit();
+        }
+
+        return restaurantRetrofit;
+    }
+
+    private void setRestaurantRetrofit() {
+        restaurantRetrofit = new Retrofit.Builder()
+                .baseUrl("http://openapi.seoul.go.kr:8088/4b64546d6862687339384d4b625366/json/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RestaurantRetrofit.class);
     }
 }

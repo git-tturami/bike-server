2c78fee0033b6fe7a21c0d257be32a65a5415104
Minji Hwang <jjparson93@gmail.com>
2019-09-20 15:56:06 +0900

    impl: getSortingStationListByDistance method returns summarized list.

--- src/main/java/com/gitturami/bikeserver/infra/bike/impl/BikeStationApiImpl.java
+++ src/main/java/com/gitturami/bikeserver/infra/bike/impl/BikeStationApiImpl.java
@@ -135,8 +135,10 @@ public class BikeStationApiImpl implements BikeStationApi {
 
         for (int i = 0; i < bikeStationResponse.rentBikeStatus.row.size(); i++) {
             BikeStationRepoLight bikeStationRepoLight = new BikeStationRepoLight();
-            bikeStationRepoLight.shared = bikeStationResponse.rentBikeStatus.row.get(i).shared;
+
+            bikeStationRepoLight.stationName = bikeStationResponse.rentBikeStatus.row.get(i).stationName;
             bikeStationRepoLight.stationId = bikeStationResponse.rentBikeStatus.row.get(i).stationId;
+            bikeStationRepoLight.shared = bikeStationResponse.rentBikeStatus.row.get(i).shared;
             bikeStationRepoLight.stationLatitude = bikeStationResponse.rentBikeStatus.row.get(i).stationLatitude;
             bikeStationRepoLight.stationLongitude = bikeStationResponse.rentBikeStatus.row.get(i).stationLongitude;
 

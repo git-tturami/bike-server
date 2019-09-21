39753e85e6a3c769106326fcba6d4c312805619a
boohyunsik <bhs9194@nate.com>
2019-09-21 20:17:29 +0900

    fix: item null check

--- src/main/java/com/gitturami/bikeserver/infra/leisure/impl/LeisureApiImpl.java
+++ src/main/java/com/gitturami/bikeserver/infra/leisure/impl/LeisureApiImpl.java
@@ -119,6 +119,10 @@ public class LeisureApiImpl implements LeisureApi {
     private List<LightLeisure> toLightItem(LeisureResponse response) {
         List<LightLeisure> list = new ArrayList<>();
         for (LeisureItem item : response.body.items) {
+            if (item == null || "".equals(item.mapx) || "".equals(item.title)
+                || item.mapx == null || item.mapy == null) {
+                continue;
+            }
             LightLeisure lightItem = new LightLeisure();
             lightItem.mapx = item.mapx;
             lightItem.mapy = item.mapy;

c953e2d3daee76c8234689fa440df6726004c0db
Minji Hwang <jjparson93@gmail.com>
2019-09-21 21:08:37 +0900

    fix: remove unused code

--- src/main/java/com/gitturami/bikeserver/infra/restaurant/RestaurantApi.java
+++ src/main/java/com/gitturami/bikeserver/infra/restaurant/RestaurantApi.java
@@ -1,6 +1,5 @@
 package com.gitturami.bikeserver.infra.restaurant;
 
-import com.gitturami.bikeserver.infra.bike.repository.BikeStationResponse;
 import com.gitturami.bikeserver.infra.restaurant.repository.RestaurantRepo;
 import com.gitturami.bikeserver.infra.restaurant.repository.RestaurantRepoLight;
 import com.gitturami.bikeserver.infra.restaurant.repository.RestaurantResponse;
@@ -12,6 +11,8 @@ public interface RestaurantApi {
 
     // getRestaurantList() returns restaurant list from start page to end page.
     RestaurantResponse getRestaurantList(int startPage, int endPage);
+
+    RestaurantRepo getRestaurantByName(String name);
 
     // getLightRestaurantList() returns summarized restaurant list from start page to end page.
     List<RestaurantRepoLight> getLightRestaurantList(int startPage, int endPage);
